2091c983f2bf45098076b69ae750300d37ba1a4f
adryel <adryel_fmr@hotmail.com>
2024-06-17 15:23:44 -0300

    commits

--- dist/controllers/FotoController.js
+++ dist/controllers/FotoController.js
@@ -1,5 +1,5 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _multer = require('multer'); var _multer2 = _interopRequireDefault(_multer);
-var _multerconfig = require('../config/multerconfig'); var _multerconfig2 = _interopRequireDefault(_multerconfig);
+var _multerconfig = require('../config/multerConfig'); var _multerconfig2 = _interopRequireDefault(_multerconfig);
 var _Foto = require('../models/Foto'); var _Foto2 = _interopRequireDefault(_Foto);
 
 const upload = _multer2.default.call(void 0, _multerconfig2.default).single('foto')
--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,5 +1,5 @@
 import multer from "multer"
-import multerConfig from "../config/multerconfig";
+import multerConfig from "../config/multerConfig";
 import Foto from '../models/Foto'
 
 const upload = multer(multerConfig).single('foto')
