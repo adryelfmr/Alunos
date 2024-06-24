0ace1d97c80df48acd333d888ce9000e2e3e98b3
adryel <adryel_fmr@hotmail.com>
2024-06-24 18:02:28 -0300

    'Adicionado'

--- dist/app.js
+++ dist/app.js
@@ -13,6 +13,21 @@ var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interop
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
 
+const whiteList = [
+  'http://react1.produtosbrnacional.com',
+  'http://localhost:3000'
+];
+
+const corsOptions = {
+  origin: function(origin, callback){
+    if(whiteList.indexOf(origin) !== -1 || !origin){
+      callback(null, true)
+    }else{
+      callback(new Error('Not allowed by cors'))
+    }
+  }
+}
+
 class App {
   constructor() {
     this.app = _express2.default.call(void 0, )
@@ -20,7 +35,7 @@ class App {
     this.routes()
   }
   middlewares() {
-    this.app.use(_cors2.default.call(void 0, ))
+    this.app.use(_cors2.default.call(void 0, corsOptions))
     this.app.use(_helmet2.default.call(void 0, ))
     this.app.use(_express2.default.urlencoded({ extended: true }))
     this.app.use(_express2.default.json())
